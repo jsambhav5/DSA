12cba34517250a7b39d7e395f2174a34554d8de6
jsambhav5 <jsambhav5@gmail.com>
2024-08-03 14:37:40 +0530

    Solved set ith bit

---
+++ 08 Bit Manipulation/Set_ith_Bit.cpp
@@ -0,0 +1,35 @@
+/*
+	Given a 32 bit unsigned integer num and an integer i
+	Perform following operations on the number -
+		1. Get ith bit
+		2. Set ith bit
+		3. Clear ith bit
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	void bitManipulation(int num, int i) {
+		cout << ((num >> (i - 1)) & 1) << " ";
+		cout << (num | (1 << (i - 1))) << " ";
+		cout << (num & ~(1 << (i - 1)));
+		// cout << ~(~num | 1 << (i - 1));
+	}
+};
+
+// Driver Code Starts
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, i;
+		cin >> n >> i;
+		Solution ob;
+		ob.bitManipulation(n, i);
+		cout << "\n";
+	}
+	return 0;
+}
+// Driver Code Ends
