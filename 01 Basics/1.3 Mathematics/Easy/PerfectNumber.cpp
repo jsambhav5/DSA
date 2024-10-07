a8c135339d9fea764d9d1ecf24d9e778f3923977
jsambhav5 <jsambhav5@gmail.com>
2024-10-08 01:09:56 +0530

    Optimal Solution for perfect number

--- 01 Basics/1.3 Mathematics/Easy/PerfectNumber.cpp
+++ 01 Basics/1.3 Mathematics/Easy/PerfectNumber.cpp
@@ -1,12 +1,24 @@
 // TUF+ : https://takeuforward.org/plus/data-structures-and-algorithm/beginner-problems/basic-maths/check-for-perfect-number
+// LeetCode 507
 
 class Solution {
 public:
-	bool isPerfect(int n) {
+	bool checkPerfectNumber(int num) {
+		// int sum = 0;
+		// for (int i = 1; i <= num / 2; i++)
+		// 	if (num % i == 0)
+		// 		sum += i;
+		// return sum == num;
+
+		if (num <= 0) return false;
 		int sum = 0;
-		for (int i = 1; i <= n / 2; i++)
-			if (n % i == 0)
+		for (int i = 1; i * i <= num; i++) {
+			if (num % i == 0) {
 				sum += i;
-		return sum == n;
+				if (i != num / i)
+					sum += num / i;
+			}
+		}
+		return sum - num == num;
 	}
 };
