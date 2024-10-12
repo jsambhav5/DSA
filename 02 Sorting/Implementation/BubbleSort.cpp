f7aa3cb08c0e664fb1bb70078517870d2f77442c
jsambhav5 <jsambhav5@gmail.com>
2024-10-12 17:16:49 +0530

    Bubble Sort

---
+++ 02 Sorting/Implementation/BubbleSort.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+	vector<int> bubbleSort(vector<int>& nums) {
+		for (int i = nums.size() - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++)
+				if (nums[j] > nums[j + 1])
+					swap(nums[j], nums[j + 1]);
+		}
+		return nums;
+	}
+};
