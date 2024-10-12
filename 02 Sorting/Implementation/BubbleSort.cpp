21a594eecf27aa91d30cf3fcd15e878467a33424
jsambhav5 <jsambhav5@gmail.com>
2024-10-12 17:53:30 +0530

    Reverse Integer

--- 02 Sorting/Implementation/BubbleSort.cpp
+++ 02 Sorting/Implementation/BubbleSort.cpp
@@ -2,9 +2,13 @@ class Solution {
 public:
 	vector<int> bubbleSort(vector<int>& nums) {
 		for (int i = nums.size() - 1; i > 0; i--) {
+			bool didSwap = false;
 			for (int j = 0; j < i; j++)
-				if (nums[j] > nums[j + 1])
+				if (nums[j] > nums[j + 1]) {
 					swap(nums[j], nums[j + 1]);
+					didSwap = true;
+				}
+			if (didSwap) break;
 		}
 		return nums;
 	}
