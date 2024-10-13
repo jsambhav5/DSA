07e9f0232887b35b316adbd03b44849a82bc75de
jsambhav5 <jsambhav5@gmail.com>
2024-10-13 21:56:06 +0530

    Rearranged

--- 03 Array/3.0 Fundamental/ReverseAnArray.cpp
+++ 03 Array/3.0 Fundamental/ReverseAnArray.cpp
@@ -2,11 +2,10 @@
 
 class Solution {
 public:
-	int countOdd(int arr[], int n) {
-		int count = 0;
-		for (int i = 0; i < n; i++)
-			if (arr[i] & 1)
-				count++;
-		return count;
+	void reverse(int arr[], int n) {
+		int i = 0, j = n - 1, temp;
+		while (i < j) {
+			swap(arr[i++], arr[j--]);
+		}
 	}
 };
