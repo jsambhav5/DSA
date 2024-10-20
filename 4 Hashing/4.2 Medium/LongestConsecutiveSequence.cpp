e51eb210baa59b42e0f613e771157d4f4e9c83ac
jsambhav5 <jsambhav5@gmail.com>
2024-10-20 19:12:11 +0530

    Longest Consecutive Sequence

--- 4 Hashing/4.2 Medium/LongestConsecutiveSequence.cpp
+++ 4 Hashing/4.2 Medium/LongestConsecutiveSequence.cpp
@@ -7,7 +7,9 @@ using namespace std;
 class Solution {
 
 public:
-	int longestConsecutive(vector<int>& nums) {
+
+	// Time Complexity - O(N LogN)
+	int longestConsecutiveSlow(vector<int>& nums) {
 		if (nums.size() == 0) {
 			return 0;
 		}
@@ -32,6 +34,34 @@ public:
 		longest = max(longest, count);
 		return longest;
 	}
+
+	// Time Complexity - O(N)
+	int longestConsecutive(vector<int>& nums) {
+		int n = nums.size();
+
+		if (n == 0) return 0;
+
+		unordered_set<int> mem;
+		int longest = 1;
+
+		for (int i = 0; i < n; i++)
+			mem.insert(nums[i]);
+
+		for (int i : mem) {
+			if (mem.find(i - 1) == mem.end()) {
+				int count = 1;
+				int x = i;
+
+				while (mem.find(x + 1) != mem.end()) {
+					x++;
+					count++;
+				}
+				longest = max(longest, count);
+			}
+		}
+
+		return longest;
+	}
 };
 
 int main() {
@@ -52,3 +82,12 @@ int main() {
 	cout << ans;
 	return 0;
 }
+
+/*
+Test Input :
+9
+0 3 7 5 8 4 6 0 1
+
+Test Output :
+9
+*/
