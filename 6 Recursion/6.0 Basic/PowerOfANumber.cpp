bed9d3858b0c135867e3a98b220ed38ee38f6b62
jsambhav5 <jsambhav5@gmail.com>
2024-10-29 15:45:24 +0530

    Power of a Number using Recursion

--- 6 Recursion/6.0 Basic/PowerOfANumber.cpp
+++ 6 Recursion/6.0 Basic/PowerOfANumber.cpp
@@ -1,3 +1,8 @@
+// TUF+ : Power of a Number using Recursion (https://takeuforward.org/plus/data-structures-and-algorithm/recursion/implementation-problems/pow(x,n))
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
 	double myPow(double x, int n) {
@@ -10,3 +15,42 @@ public:
 		return 1;
 	}
 };
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		double x;
+		int n;
+		cin  >> x >> n;
+
+		Solution sol;
+
+		double ans = sol.myPow(x, n);
+
+		cout << ans << endl;
+	}
+
+	return 0;
+}
+
+/*
+Test Input : 
+3
+
+2.0000
+10
+
+2.0000
+-2
+
+2.5000
+2
+
+Test Output : 1
+1024.0000
+0.2500
+6.25
+
+*/ 
