c6edfd1014ca15cdb7e7361241f231249ac0ec92
jsambhav5 <jsambhav5@gmail.com>
2024-11-01 19:34:13 +0530

    Find the Smallest Divisor Given a Threshold

--- 2 Binary Search/2.2 Medium/FindSmallestDivisorGivenThreshold.cpp
+++ 2 Binary Search/2.2 Medium/FindSmallestDivisorGivenThreshold.cpp
@@ -1,28 +1,103 @@
+// LeetCode : 1283 - Find the Smallest Divisor Given a Threshold (https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold)
+// TUF+ : Find the smallest divisor (https://takeuforward.org/plus/data-structures-and-algorithm/binary-search/on-answers/find-the-smallest-divisor)
+
+/*
+Approach:Binary Search On Answers
+Instead of checking for every divisor from 1 to max of array, we can just check for (min + max) / 2
+where min starts with 1 and max starts with bananas in biggest pile
+if possible solution exists, then ans is in left half else ans is in right half
+
+TC: O(log(max)*N)
+SC: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 private:
-	int sumByD(vector<int>& nums, int threshold) {
-		int n = nums.size();
+	bool possible(vector<int>& nums, int limit, int div) {
 		int sum = 0;
+		int n = nums.size();
+
 		for (int i = 0; i < n; i++) {
-			sum += ceil((double)(nums[i]) / (double)(threshold));
+			sum += nums[i] / div;
+
+			if (nums[i] % div != 0) sum++;
 		}
-		return sum;
+
+		return sum <= limit;
 	}
 
 public:
-	int smallestDivisor(vector<int>& nums, int threshold) {
-		int n = nums.size();
-		if (n > threshold) return -1;
-		int low = 1, high = *max_element(nums.begin(), nums.end());
+	int smallestDivisor(vector<int>& nums, int limit) {
+		int low = 1, high = INT_MIN;
+		int ans = -1;
+		for (int i : nums)
+			high = max(high, i);
+
 		while (low <= high) {
 			int mid = (low + high) / 2;
-			if (sumByD(nums, mid) <= threshold) {
+
+			if (possible(nums, limit, mid)) {
+				ans = mid;
 				high = mid - 1;
 			}
-			else {
-				low = mid + 1;
-			}
+			else low = mid + 1;
 		}
-		return low;
+
+		return ans;
 	}
 };
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		vector<int> arr;
+
+		for (int i = 0; i < n; i++) {
+			int input;
+			cin >> input;
+			arr.push_back(input);
+		}
+
+		int limit;
+		cin >> limit;
+
+		Solution sol;
+
+		int ans = sol.smallestDivisor(arr, limit);
+
+		cout << ans << endl;
+	}
+
+	return 0;
+}
+
+/*
+Test Input : 
+3
+
+5
+1 2 3 4 5
+8
+
+4
+8 4 2 3
+10
+
+4
+8 4 2 3
+4
+
+Test Output : 1
+3
+2
+8
+
+*/ 
