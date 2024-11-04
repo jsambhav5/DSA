510503fca04b06d5e898d76e0fbfc7cdfbab5488
jsambhav5 <jsambhav5@gmail.com>
2024-11-04 11:59:45 +0530

    Find Row with Maximum Number of 1's

---
+++ 2 Binary Search/2.1 Easy (Fundamental)/2-D Arrays/FindRowWithMaximumNumberOf1s.cpp
@@ -0,0 +1,109 @@
+// TUF+ : Find row with maximum 1's (https://takeuforward.org/plus/data-structures-and-algorithm/binary-search/2d-arrays/find-row-with-maximum-1's)
+
+/*
+Approach:
+Used Binary Search to ount number of 1s in rach row
+
+TC: O(N*logM)
+SC: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+	int countOnes(int n, vector<int>& arr, int res) {
+		int count = 0;
+		int low = 0;
+		int high = arr.size() - 1;
+
+		while (low <= high) {
+			int mid = (low + high) / 2;
+
+			if (arr[mid] == 0) {
+				if (n - mid < res) return -1;
+				low = mid + 1;
+			}
+			else {
+				high = mid - 1;
+				count = n - mid;
+			};
+		}
+
+		return count;
+	}
+
+public:
+	int rowWithMax1s(vector<vector<int>>& mat) {
+		int n = mat.size();
+		int m = mat[0].size();
+
+		int res = -1;
+		int maxOne = 0;
+
+		for (int i = 0; i < n; i++) {
+			int count = countOnes(m, mat[i], maxOne);
+			if (count > maxOne) {
+				maxOne = count;
+				res = i;
+			}
+		}
+
+		return res;
+	}
+};
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n, m;
+		cin >> n >> m;
+
+		vector<vector<int>> matrix;
+		vector<int> arr;
+
+		for (int i = 0; i < n; i++) {
+			for (int i = 0; i < m; i++) {
+				int input;
+				cin >> input;
+				arr.push_back(input);
+			}
+			matrix.push_back(arr);
+			arr.clear();
+		}
+
+		Solution sol;
+		int res = sol.rowWithMax1s(matrix);
+		cout << res << endl;
+	}
+
+	return 0;
+}
+
+/*
+Test Input : 
+3
+
+3 3
+1 1 1
+0 0 1
+0 0 0
+
+2 2
+0 0
+0 0
+
+3 3
+0 0 1
+0 1 1
+0 1 1
+
+Test Output : 1
+0
+-1
+1
+
+*/ 
