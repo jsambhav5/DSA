ad20f3303f33d0ef4e8aef730ab96a3a2620e473
Sambhav Jain <jsambhav5@gmail.com>
2024-11-06 22:18:14 +0530

    Create DeletionOfTheHeadOfLL.cpp

---
+++ 8 LinkedList/8.0 Implementation/DeletionOfTheHeadOfLL.cpp
@@ -0,0 +1,29 @@
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode* deleteHead(ListNode* &head) {
+        if(head == nullptr || head->next == nullptr)
+            return nullptr;
+        head->val = head->next->val;
+        head->next = head->next->next;
+        return head;
+    }
+};
