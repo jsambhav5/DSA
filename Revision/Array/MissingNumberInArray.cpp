933bd170eac94227a2a43c494af78ae7b51c5f06
jsambhav5 <jsambhav5@gmail.com>
2025-02-02 20:30:05 +0530

    Missing Number

--- Revision/Array/MissingNumberInArray.cpp
+++ Revision/Array/MissingNumberInArray.cpp
@@ -16,16 +16,13 @@ class Solution {
 public:
 	int missingNumber(vector<int>& nums) {
 		int n = nums.size();
-		int xor_ = 0;
-		for (int i = 0; i < n; i++) {
-			xor_ = xor_ ^ nums[i] ^ i;
+		int xor1 = 0, xor2 = nums[0];
+		for (int i = 1; i < n; i++) {
+			xor1 ^= i;
+			xor2 ^= nums[i];
 		}
-		return xor_ ^ n;
-		// int sum = 0;
-		// for(int i = 0; i < n; i++) {
-		//     sum += nums[i];
-		// }
-		// return ((n * (n + 1)) / 2) - sum ;
+		xor1 ^= n;
+		return xor1 ^ xor2;
 	}
 };
 
---
+++ Revision/Array/SingleNumber.cpp
@@ -0,0 +1,70 @@
+// LeetCode : 136 - Single Number (https://leetcode.com/problems/single-number/description/)
+// TUF+ : Single Number - I ( https://takeuforward.org/plus/dsa/bit-manipulation/problems/single-number---i )
+
+/*
+Approach:
+Used XOR bitwise operation to solve this
+
+TC: O(N)
+SC: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		int res = 0;
+		for (int i : nums) {
+			res ^= i;
+		}
+		return res;
+	}
+};
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		vector<int> arr;
+
+		for (int i = 0; i < n; i++) {
+			int input;
+			cin >> input;
+			arr.push_back(input);
+		}
+
+		Solution sol;
+
+		int res = sol.singleNumber(arr);
+
+		cout << res << endl;
+	}
+
+	return 0;
+}
+
+/*
+Test Input :
+3
+
+3
+2 2 1
+
+5
+4 1 2 1 2
+
+1
+1
+
+Test Output : 1
+OUTPUT
+1
+4
+1
+*/
