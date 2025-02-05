1f2e46d0962581a1ae4707778aca83c21d4c4b4b
jsambhav5 <jsambhav5@gmail.com>
2025-02-05 16:01:31 +0530

    Maximum Nesting Depth of the Parentheses

---
+++ 5 String/5.1 Easy/MaximumNestingDepthOfTheParentheses.cpp
@@ -0,0 +1,63 @@
+// LeetCode : 1614 - Maximum Nesting Depth of the Parentheses (https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/description/)
+
+/*
+Approach:
+Used Linear Traversal to solve this
+
+TC: O(N)
+SC: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	int maxDepth(string s) {
+		int maxDepth = 0;
+		int count = 0;
+
+		for (auto ch : s) {
+			if (ch == '(') {
+				count++;
+				maxDepth = max(maxDepth, count);
+			}
+			else if (ch == ')') {
+				count--;
+			}
+		}
+
+		return maxDepth;
+	}
+};
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		string s;
+		cin >> s;
+
+		Solution sol;
+		int res = sol.maxDepth(s);
+
+		cout << res << endl;
+	}
+
+	return 0;
+}
+
+/*
+Test Input :
+3
+(1+(2*3)+((8)/4))+1
+(1)+((2))+(((3)))
+()(())((()()))
+
+Test Output : 1
+3
+3
+3
+
+*/
