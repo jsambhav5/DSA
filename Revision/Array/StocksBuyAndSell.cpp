782ead2e9b4576cbc6d9e0212ab554a7326328d9
jsambhav5 <jsambhav5@gmail.com>
2025-02-14 08:49:42 +0530

    Stocks Buy and Sell

--- Revision/Array/StocksBuyAndSell.cpp
+++ Revision/Array/StocksBuyAndSell.cpp
@@ -16,15 +16,16 @@ class Solution {
 public:
 	int maxProfit(vector<int>& prices) {
 		int min = prices[0];
-		int profit = 0;
-		for (int i = 1; i < prices.size(); i++) {
-			int current = prices[i];
-			if (current < min)
-				min = current;
-			else if (profit < current - min)
-				profit = current - min;
+		int maxProfit = 0;
+
+		for (int element : prices) {
+			if (element < min)
+				min = element;
+			else
+				maxProfit = max(maxProfit, element - min);
 		}
-		return profit;
+
+		return maxProfit;
 	}
 };
 
