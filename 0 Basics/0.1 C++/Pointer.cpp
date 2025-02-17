2b8a84712d9d20aec756a4e420693c67bade4697
jsambhav5 <jsambhav5@gmail.com>
2025-02-17 11:55:17 +0530

    Updated Outputs in Pointer.cpp

--- 0 Basics/0.1 C++/Pointer.cpp
+++ 0 Basics/0.1 C++/Pointer.cpp
@@ -3,28 +3,45 @@ using namespace std;
 
 int main(int argc, const char** argv) {
 
-    int t;
-    cin >> t;
-    char name[] = "Sambhav Jain";
-    char* a = name;
-    cout << t << "\n";
-    cout << name << "\n";
-    cout << a << "\n";
-    cout << *a << "\n";
-    cout << endl;
+	int t;
+	cin >> t; // 3
+	char name[] = "Sambhav Jain";
+	char* a = name;
+	cout << t << "\n";  // 3
+	cout << name << "\n"; // Sambhav Jain
+	cout << a << "\n";  // Sambhav Jain
+	cout << *a << "\n"; // S
+	cout << endl;
 
-    int x = *a;
-    cout << x << "\n";
-    cout << int(*a) << "\n";
-    cout << endl;
+	int x = *a;
+	cout << x << "\n"; // 83
+	cout << int(*a) << "\n"; // 83
+	cout << endl;
 
-    int z[] = { 10,11,12,14 };
-    int* y = z;
-    cout << z << "\n";
-    cout << y << "\n";
-    cout << *y << "\n";
-    cout << sizeof(z) << "\t" << sizeof(y);
-    cout << endl;
+	int z[] = { 10,11,12,14 };
+	int* y = z;
+	cout << z << "\n"; // 0x61fee0
+	cout << y << "\n"; // 0x61fee0
+	cout << *y << "\n"; // 10
+	cout << sizeof(z) << "\t" << sizeof(y); // 16 4
+	cout << endl;
 
-    return 0;
+	return 0;
 }
+
+/*
+OUTPUT:
+3
+Sambhav Jain
+Sambhav Jain
+S
+
+83
+83
+
+0x61fee0
+0x61fee0
+10
+16	4
+
+*/
