8488aa5d3ab655830231a002ed1f684750059add
jsambhav5 <jsambhav5@gmail.com>
2025-02-21 13:49:07 +0530

    4 Sum

--- Revision/Array/FourSum.cpp
+++ Revision/Array/FourSum.cpp
@@ -32,11 +32,11 @@ public:
 				int l = n - 1;
 
 				while (k < l) {
-					long long sum = nums[i] + nums[j];
-					sum += nums[k];
-					sum += nums[l];
+					long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
 
-					if (sum == target) {
+					if (sum < target) k++;
+					else if (sum > target) l--;
+					else {
 						vector<int> temp = { nums[i], nums[j], nums[k], nums[l] };
 						ans.push_back(temp);
 
@@ -45,8 +45,6 @@ public:
 						while (k < l && nums[k] == nums[k - 1]) k++;
 						while (k < l && nums[l] == nums[l + 1]) l--;
 					}
-					else if (sum < target) k++;
-					else l--;
 				}
 			}
 		}
