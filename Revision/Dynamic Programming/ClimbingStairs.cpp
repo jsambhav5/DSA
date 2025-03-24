ff83a7f7e7e9ef652abfb4917632ba0d47101395
jsambhav5 <jsambhav5@gmail.com>
2025-03-24 06:41:57 +0530

    Climbing stairs

--- Revision/Dynamic Programming/ClimbingStairs.cpp
+++ Revision/Dynamic Programming/ClimbingStairs.cpp
@@ -17,17 +17,17 @@ using namespace std;
 
 class Solution {
 public:
-    int climbStairs(int n) {
-        int prev2 = 1;
-        int prev = 1;
+	int climbStairs(int n) {
+		int prev2 = 1;
+		int prev = 1;
 
-        for (int i = 2; i <= n; i++) {
-            int cur_i = prev2 + prev;
-            prev2 = prev;
-            prev = cur_i;
-        }
-        return prev;
-    }
+		for (int i = 2; i <= n; i++) {
+			int cur_i = prev2 + prev;
+			prev2 = prev;
+			prev = cur_i;
+		}
+		return prev;
+	}
 };
 
 int main(int argc, const char** argv) {
