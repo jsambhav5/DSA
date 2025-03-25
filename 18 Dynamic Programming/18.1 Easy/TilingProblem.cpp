ca904f9baae768724409caddf2fb0991fa0ab458
jsambhav5 <jsambhav5@gmail.com>
2025-03-25 22:11:23 +0530

    Tiling Problem

--- 18 Dynamic Programming/18.1 Easy/TilingProblem.cpp
+++ 18 Dynamic Programming/18.1 Easy/TilingProblem.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<chrono>
 using namespace std;
-using namespace std::chrono;
 
 int countingWays(int N, int* mem) {
 	if (N == 1 || N == 2)
@@ -16,10 +15,6 @@ int main(int argc, const char** argv) {
 	int N;
 	cin >> N;
 	int mem[N] = { 0 };
-	auto start = high_resolution_clock::now();
 	cout << countingWays(N, mem) << endl;
-	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(stop - start);
-	cout << duration.count() << " microseconds" << endl;
 	return 0;
 }
