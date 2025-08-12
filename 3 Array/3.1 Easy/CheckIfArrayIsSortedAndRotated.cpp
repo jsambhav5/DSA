01847bd7fc4a9ccdf7078e65df92902cd36efa18
Sambhav Jain <jsambhav5@gmail.com>
2025-08-12 10:15:46 +0530

    optimised

--- 3 Array/3.1 Easy/CheckIfArrayIsSortedAndRotated.cpp
+++ 3 Array/3.1 Easy/CheckIfArrayIsSortedAndRotated.cpp
@@ -18,20 +18,14 @@ using namespace std;
 class Solution {
 public:
 	bool check(vector<int>& nums) {
-		int count = 0;
 		int n = nums.size();
+        int count = nums[0] < nums[n - 1];
 
 		for (int i = 1; i < n; i++) {
-			if (nums[i] < nums[i - 1])
-				count++;
-			if (count >= 2)
-				return false;
+			count += (nums[i] < nums[i - 1]);
+			if (count == 2) break;
 		}
-		if (nums[0] < nums[n - 1])
-			count++;
-		if (count < 2)
-			return true;
-		return false;
+		return count < 2;
 	}
 };
 
